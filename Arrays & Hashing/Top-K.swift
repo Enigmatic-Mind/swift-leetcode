c14a9936c18a61dbfdd0807abf3858d3e75793ca
enigmatic-mind <enigmatic-mind@users.noreply.github.com>
2026-05-22 10:16:17 -0700

    top k bucket sort

--- Arrays & Hashing/Top-K.swift
+++ Arrays & Hashing/Top-K.swift
@@ -7,13 +7,42 @@
 
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+            var freqDict = [Int : Int]()
+            for num in nums {
+                freqDict[num, default: 0] += 1
+            }
+            
+            let vals = freqDict.keys.sorted { freqDict[$0]! > freqDict[$1]! }
+            return Array(vals[..<k])
+        }
+    
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
         var freqDict = [Int : Int]()
         for num in nums {
             freqDict[num, default: 0] += 1
         }
         
-        let vals = freqDict.keys.sorted { freqDict[$0]! > freqDict[$1]! }
-        return Array(vals[..<k])
+        var buckets = Array(repeating: [Int](), count: nums.count + 1)
+        for (num, freq) in freqDict {
+            buckets[freq].append(num)
+        }
+        
+        var topK = [Int]()
+
+        for i in buckets.indices.reversed() {
+            for val in buckets[i] {
+                if topK.count < k {
+                    topK.append(val)
+                }
+            }
+            
+            if topK.count == k {
+                return topK
+            }
+            
+        }
+        
+        return topK
     }
 }
 
@@ -21,5 +50,10 @@ class Solution {
  First Answer
  Time: 15 mins
  Time Complexity: O(n log n) -> sorting dominates
- Space Complexity: O(n) -> storing a multiple of n values
+ Space Complexity: O(n) -> dictionary and sorted array each store up to n elements, combined space is still linear.
+ 
+ Bucket sort
+ Time: 25 mins
+ Time Complexity: O(n) -> going through n elements in freq counting, dict to buckets, and top k
+ Space Complexity: O(n) -> data stored in dict, buckets array, and top k.
  */
