da1cf1eb3b0bf1f11b246c90784726cd7a798136
enigmatic-mind <enigmatic-mind@users.noreply.github.com>
2026-06-10 12:26:51 -0700

    min window substring

--- Sliding Window/Min-Window-Substring.swift
+++ Sliding Window/Min-Window-Substring.swift
@@ -7,24 +7,45 @@
 
 class Solution {
     func minWindow(_ s: String, _ t: String) -> String {
-        convert t for a freq dict
-
-        Left = 0
-        seen  = [character: int]()
-        minString=""
-
-        for i in array.indices {
-        seen[array[i], default: 0] += 1
-        while seen contains all keys + frequencies > t's dict {
-        update minString if needed
-        remove left
-        Left += 1
+        var needCount = [Character: Int]()
+        var haveCount = [Character: Int]()
+        var need = 0
+        var have = 0
+        var result = [Character]()
+        var left = 0
+        
+        for char in t {
+            needCount[char, default: 0] += 1
         }
+        need = needCount.count
+        
+        let sChars = Array(s)
+        for i in 0..<sChars.count {
+            let currChar = sChars[i]
+            
+            haveCount[currChar, default: 0] += 1
+            if needCount[currChar] != nil && haveCount[currChar, default: 0] == needCount[currChar] {
+                have += 1
+            }
+            
+            while have == need {
+                // update min substring
+                if result.isEmpty || (i - left + 1) < result.count {
+                    result = Array(sChars[left ... i])
+                }
+                
+                haveCount[sChars[left], default: 0] -= 1
+                if needCount[sChars[left]] != nil && haveCount[sChars[left], default: 0] < needCount[sChars[left], default: 0] {
+                    have -= 1
+                }
+                
+                left += 1
+            }
         }
-        return minstring
-
+        return String(result)
     }
 }
 
 
-
+// Time complexity: O(m + n) -> need count and sliding window over s
+// Space complexity: O(n) -> dicts are 26 characters but in swift we need Array(s) and result so O(n)
