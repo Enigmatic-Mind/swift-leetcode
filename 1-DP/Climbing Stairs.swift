7cc4ab7782cc7546a57c6457a040a935e742ff05
enigmatic-mind <enigmatic-mind@users.noreply.github.com>
2026-06-29 15:03:44 -0700

    climb stairs

---
+++ 1-DP/Climbing Stairs.swift
@@ -0,0 +1,22 @@
+//
+//  Climbing Stairs.swift
+//  NeetCode
+//
+//  Created by Francisco on 6/29/26.
+//
+
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        guard n > 1 else { return 1 }
+
+        var dp = Array(repeating: 0, count: n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in 2...n {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+        
+        return dp[n]
+    }
+}
