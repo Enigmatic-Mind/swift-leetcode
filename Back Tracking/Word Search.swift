dbfbc158764cb425d333ba9b01a5f64d8315af64
enigmatic-mind <enigmatic-mind@users.noreply.github.com>
2026-07-02 13:12:11 -0700

    add: word search sol - O(L) space optimization

--- Back Tracking/Word Search.swift
+++ Back Tracking/Word Search.swift
@@ -6,7 +6,7 @@
 //
 
 class Solution {
-    func exist(_ board: [[Character]], _ word: String) -> Bool {
+    func nonoptimal_exist(_ board: [[Character]], _ word: String) -> Bool {
         var wordChars = Array(word)
         
         var seenChar = Set<[Int]>()
@@ -31,6 +31,40 @@ class Solution {
             for col in 0..<board[row].count {
                 if board[row][col] == wordChars[0] {
                     if backtrack(row, col, 0, seenChar) { return true }
+                }
+            }
+        }
+        return false
+    }
+    
+    func exist(_ board: [[Character]], _ word: String) -> Bool {
+        var wordChars = Array(word)
+        
+        var seenChar = Set<[Int]>()
+        
+        func backtrack(_ y: Int, _ x: Int, _ index: Int) -> Bool {
+            if index >= wordChars.count { return true }
+            
+            if (y < 0) || (y >= board.count) || (x < 0) || (x >= board[y].count) || (seenChar.contains([y, x])) || (board[y][x] != wordChars[index]) {
+                return false
+            }
+            
+            seenChar.insert([y, x])
+            
+            let found = backtrack(y, x-1, index+1) ||  // left
+                backtrack(y, x+1, index+1) ||         // right
+                backtrack(y-1, x, index+1) ||         // up
+                backtrack(y+1, x, index+1)            // down
+            
+            seenChar.remove([y, x])
+            
+            return found
+        }
+        
+        for row in 0..<board.count {
+            for col in 0..<board[row].count {
+                if board[row][col] == wordChars[0] {
+                    if backtrack(row, col, 0) { return true }
                 }
             }
         }
